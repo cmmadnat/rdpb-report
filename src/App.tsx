a5db7656484d1816541f841af3743680e1ca00a3
cmmadnat <cmmadnat@gmail.com>
2020-03-18 20:35:25 +0700

    add all topics

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Navbar, Container } from 'react-bootstrap'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar bg="light">
+        <Navbar.Brand href="#home">รายงานศูนย์เรียนรู้</Navbar.Brand>
+      </Navbar>
+      <Container>
+        <h1>รายงานศูนย์เรียนรู้</h1>
+        <h2>รายงานศูนย์เรียนรู้ ตามสถานะข้อมูล</h2>
+        <h3>รายภาค</h3>
+        <h3>รายจังหวัด</h3>
+        <h2>รายงานศูนย์เรียนรู้ ตามสถานะศูนย์เรียนรู้ พื้นที่เป้าหมาย</h2>
+        <h3>รายภาค</h3>
+        <h3>รายจังหวัด</h3>
+        <h2>รายงานศูนย์เรียนรู้ตามประเภท</h2>
+        <h3>รายภาค</h3>
+        <h3>รายจังหวัด</h3>
+        <h2>รายงานศูนย์เรียนรู้ตามหน่วยงาน</h2>
+        <h3>รายภาค</h3>
+        <h3>รายจังหวัด</h3>
+        <h2>รายงานศูน์เรียนรู้ตามศูนย์พัฒนา</h2>
+      </Container>
+    </>
   );
 }
 
