721d2b1aa9c0485b801e138f4fac36a024b2f0ce
cmmadnat <cmmadnat@gmail.com>
2020-04-07 17:11:30 +0700

    add sufficiency region

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,10 @@
 /* eslint-disable react/jsx-no-undef */
 import React from 'react';
-import { Navbar, Container, Accordion, Card, Button } from 'react-bootstrap'
-import DataTable from 'react-data-table-component'
+import { Navbar, Container, Accordion, Card, Button, Tabs, Tab } from 'react-bootstrap'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//@ts-ignore
-import DataTableExtensions from 'react-data-table-component-extensions';
+import SufficiencyReport from './sufficiency-region-report';
 
-interface RegionReport {
-  id: number;
-  name: string;
-  complete: number;
-  incomplete: number;
-  total: number;
-}
-const data = [
-  { id: 1, name: 'เหนือ', complete: 1982, incomplete: 1982, total: 1982 },
-  { id: 2, name: 'ตะวันออกเฉียงเหนือ', complete: 1982, incomplete: 1982, total: 1982 },
-  { id: 3, name: 'กลาง', complete: 1982, incomplete: 1982, total: 1982 },
-  { id: 4, name: 'ใต้', complete: 1982, incomplete: 1982, total: 1982 },
-];
-const calculateSum = (data: RegionReport[]) => {
-  const complete = data.map(it => it.complete).reduce((t, c) => t + c)
-  const incomplete = data.map(it => it.incomplete).reduce((t, c) => t + c)
-  const total = data.map(it => it.total).reduce((t, c) => t + c)
-  const output = { id: 99, name: 'รวม', complete, incomplete, total } as RegionReport
-  return output
-}
-const data2 = [...data, calculateSum(data)]
-const columns = [
-  {
-    name: 'ภาค',
-    selector: 'name',
-    sortable: true,
-  },
-  {
-    name: 'ครบถ้วน',
-    selector: 'complete',
-    sortable: true,
-    right: true,
-  },
-  {
-    name: 'ไม่ครบถ้วน',
-    selector: 'incomplete',
-    sortable: true,
-    right: true,
-  },
-  {
-    name: 'รวม',
-    selector: 'total',
-    sortable: true,
-    right: true,
-  },
-];
 
 function App() {
   return (
@@ -61,86 +13,60 @@ function App() {
         <Navbar.Brand href="#home">รายงานศูนย์เรียนรู้</Navbar.Brand>
       </Navbar>
       <Container>
-        <h1>รายงานศูนย์เรียนรู้</h1>
 
-        <Accordion defaultActiveKey="0">
-          <Card>
-            <Card.Header>
-              <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                <span>รายงานศูนย์เรียนรู้ ตามสถานะข้อมูล</span>
-              </Accordion.Toggle>
-            </Card.Header>
-            <Accordion.Collapse eventKey="0">
-              <Card.Body>
-                <h3>รายภาค</h3>
-                <DataTableExtensions
-                  columns={columns}
-                  data={data2}
-                  exportHeader={true}
-                >
-                  <DataTable
-                    data={[]}
-                    title="รายงานศูนย์เรียนรู้ ตามสถานะข้อมูล รายภาค"
-                    columns={columns}
-                    striped={true}
-                    highlightOnHover={true}
-                  />
-                </DataTableExtensions>
-                <h3>รายจังหวัด</h3>
-              </Card.Body>
-            </Accordion.Collapse>
-          </Card>
-          <Card>
-            <Card.Header>
-              <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                รายงานศูนย์เรียนรู้ ตามสถานะศูนย์เรียนรู้ พื้นที่เป้าหมาย
+        <Tabs defaultActiveKey="home" id="uncontrolled-tab-example" style={{ marginTop: 20 }}>
+          <Tab eventKey="home" title="เศรษฐกิจพอเพียง">
+            <Accordion defaultActiveKey="0">
+              <Card>
+                <Card.Header>
+                  <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                    <span>รายงานศูนย์เรียนรู้ ตามสถานะข้อมูล</span>
+                  </Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>
+                    <h3>รายภาค</h3>
+                    <SufficiencyReport type={'status'} />
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Card.Header>
+                  <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                    <span>รายงานศูนย์เรียนรู้ ตามสถานะศูนย์เรียนรู้</span>
+                  </Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="1">
+                  <Card.Body>
+                    <h3>รายภาค</h3>
+                    <SufficiencyReport type={'studyCenter'} />
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Card.Header>
+                  <Accordion.Toggle as={Button} variant="link" eventKey="2">
+                    รายงานศูนย์เรียนรู้ตามประเภท
       </Accordion.Toggle>
-            </Card.Header>
-            <Accordion.Collapse eventKey="1">
-              <Card.Body>
-                <h3>รายภาค</h3>
-                <h3>รายจังหวัด</h3>
-              </Card.Body>
-            </Accordion.Collapse>
-          </Card>
-          <Card>
-            <Card.Header>
-              <Accordion.Toggle as={Button} variant="link" eventKey="2">
-                รายงานศูนย์เรียนรู้ตามประเภท
-      </Accordion.Toggle>
-            </Card.Header>
-            <Accordion.Collapse eventKey="2">
-              <Card.Body>
-                <h3>รายภาค</h3>
-                <h3>รายจังหวัด</h3>
-              </Card.Body>
-            </Accordion.Collapse>
-          </Card><Card>
-            <Card.Header>
-              <Accordion.Toggle as={Button} variant="link" eventKey="3">
-                รายงานศูนย์เรียนรู้ตามหน่วยงาน
-      </Accordion.Toggle>
-            </Card.Header>
-            <Accordion.Collapse eventKey="3">
-              <Card.Body>
-                <h3>รายภาค</h3>
-                <h3>รายจังหวัด</h3>
-              </Card.Body>
-            </Accordion.Collapse>
-          </Card><Card>
-            <Card.Header>
-              <Accordion.Toggle as={Button} variant="link" eventKey="4">
-                รายงานศูน์เรียนรู้ตามศูนย์พัฒนา
-                 </Accordion.Toggle>
-            </Card.Header>
-            <Accordion.Collapse eventKey="4">
-              <Card.Body>
-                <h3>รายภาค</h3>
-                <h3>รายจังหวัด</h3>
-              </Card.Body>
-            </Accordion.Collapse>
-          </Card>
-        </Accordion>
+                </Card.Header>
+                <Accordion.Collapse eventKey="2">
+                  <Card.Body>
+                    <h3>รายภาค</h3>
+                    <h3>รายจังหวัด</h3>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+
+            </Accordion>
+          </Tab>
+          <Tab eventKey="profile" title="ตามแนวพระราชดำริ">
+
+          </Tab>
+          <Tab eventKey="contact" title="ศูนย์เรียนรู้หน่วยงานต่างๆ" >
+
+          </Tab>
+        </Tabs>
+
       </Container>
     </>
   );
---
+++ src/sufficiency-region-report.tsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from 'react'
+import DataTable from 'react-data-table-component'
+import request from 'superagent'
+//@ts-ignore
+import DataTableExtensions from 'react-data-table-component-extensions';
+
+interface RegionReport {
+  id: number;
+  name: string;
+  complete: number;
+  incomplete: number;
+  total: number;
+}
+// const data2 = [...data, calculateSum(data)]
+
+interface RegionReportResponse {
+  name: string
+  complete: number
+  inComplete: number
+  studyCenter: number
+  targetArea: number
+  total: number
+}
+
+const SufficiencyReport = ({ type }: { type: 'status' | 'studyCenter' }) => {
+  const [data, setData] = useState<RegionReport[]>([])
+  const columns = [
+    {
+      name: 'ภาค',
+      selector: 'name',
+      sortable: true,
+    },
+    {
+      name: type === 'status' ? 'ครบถ้วน' : 'ศูนย์เรียนรู้',
+      selector: 'complete',
+      sortable: true,
+      right: true,
+    },
+    {
+      name: type === 'status' ? 'ไม่ครบถ้วน' : 'พื้นที่เป้าหมาย',
+      selector: 'incomplete',
+      sortable: true,
+      right: true,
+    },
+    {
+      name: 'รวม',
+      selector: 'total',
+      sortable: true,
+      right: true,
+    },
+  ];
+  useEffect(() => {
+    request.get('https://rdpb-2.appspot.com/report2/sufficiencyReport/region').then(data => data.body)
+      .then((data: RegionReportResponse[]) => {
+        const d = data.map((it, index) => {
+          const c: RegionReport = type === 'status' ?
+            { id: index + 1, name: it.name, complete: it.complete, incomplete: it.inComplete, total: it.total } :
+            { id: index + 1, name: it.name, complete: it.studyCenter, incomplete: it.targetArea, total: it.total }
+          return c
+        })
+        setData(d)
+      })
+  })
+
+  return (
+    <>
+      <DataTableExtensions
+        columns={columns}
+        data={data}
+        exportHeader={true}
+      >
+        <DataTable
+          data={[]}
+          title="รายงานศูนย์เรียนรู้ ตามประเภทศูนย์เรียนรู้ รายภาค"
+          columns={columns}
+          striped={true}
+          highlightOnHover={true}
+        />
+      </DataTableExtensions>
+    </>
+  )
+}
+export default SufficiencyReport
